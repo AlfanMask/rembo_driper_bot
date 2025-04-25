40736963bae5086497a3b7400967bddfe6ee3ffc
AlfanMask <afanh123123@gmail.com>
2025-04-25 14:17:38 +0700

    fix: rembo add merlyn group

--- constants/groups.py
+++ constants/groups.py
@@ -9,12 +9,14 @@ group_chat_id_uns = os.getenv("GROUP_CHAT_ID_UNS")
 group_chat_id_ums = os.getenv("GROUP_CHAT_ID_UMS")
 group_chat_id_uny = os.getenv("GROUP_CHAT_ID_UNY")
 group_chat_id_uns_wa_kampusku = os.getenv("GROUP_CHAT_ID_UNS_WA_KAMPUSKU") # added for fun group
+group_chat_id_merlyn = os.getenv("GROUP_CHAT_ID_MERLYN") # added for fun group with merlyn
 
 group_chat_ids: Final[dict] = {
     univs.uns: group_chat_id_uns,
     univs.ums: group_chat_id_ums,
     univs.uny: group_chat_id_uny,
     "uns_wa_kampusku": group_chat_id_uns_wa_kampusku, # added for fun group
+    "group_chat_id_merlyn": group_chat_id_merlyn, # added for fun group with merlyn
 }
 
 # get group_chat_rembo_topic_ids envs
